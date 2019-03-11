1e15b7db278cd9f0d492738edbe0ab54551eea62
bbugieda <brian.bugieda@gmail.com>
2019-03-10 20:14:31 -0400

    added comments to code to increase readability

--- src/Model.java
+++ src/Model.java
@@ -1,4 +1,6 @@
-/**
+/** Brian Bugieda and Max Luu
+ * 	Lab 4
+ * 
  * Model: Contains all the state and logic
  * Does not contain anything about images or graphics, must ask view for that
  *
@@ -10,6 +12,7 @@
  **/
 
 public class Model {
+	
 	private int frameWidth;
     private int frameHeight;
     private int imgWidth;
@@ -18,21 +21,59 @@ public class Model {
     private int yM = 1;
     private int xloc = 0;
     private int yloc = 0;
+    
     private final int xIncr = 8;
     private final int yIncr = 2;
     
-    final private int NE = 3;
-    final private int NW = 4;
-    final private int SE = 0;
-    final private int SW = 6;
+    private final int NE = 3;
+    private final int NW = 4;
+    private final int SE = 0;
+    private final int SW = 6;
     
     private int direction;
     
     public Model(int w, int h, int iw, int ih) {
+    	
     	this.frameWidth = w;
     	this.frameHeight = h;
     	this.imgWidth = iw;
     	this.imgHeight = ih;
+    }
+    
+    /**
+     * Updates the location and direction of the orc
+     * Checks where the orc is on the screen and changes direction when it nears any edge
+     */
+    public void updateLocationAndDirection() {
+    	xloc+=(xIncr*xM);
+    	yloc+=(yIncr*yM);
+    	
+    	if (xloc + imgWidth >= frameWidth) {
+			xM = -1;
+		} else if (xloc + (frameHeight / 10) <= 0) {
+			xM = 1;
+		}
+    	
+    	if (yloc + imgHeight >= frameHeight) {
+			yM = -1;
+		} else if (yloc + (frameHeight / 10) <= 0) {
+			yM = 1;
+		}
+    	
+    	if (xM == 1 && yM == 1) {
+    		direction = SE;
+    	}
+    	
+    	if (xM == -1 && yM == -1) {
+    		direction = NW;
+    	}
+    	if (xM == 1 && yM == -1) {
+    		direction = NE;
+    	}
+    	
+    	if (xM == -1 && yM == 1){
+    		direction = SW;
+    	}
     }
     
     public int getX() {
@@ -71,35 +112,5 @@ public class Model {
     	yloc = y;
     }
     
-    public void updateLocationAndDirection() {
-    	xloc+=(xIncr*xM);
-    	yloc+=(yIncr*yM);
-    	
-    	if (xloc + imgWidth >= frameWidth) {
-			xM = -1;
-		} else if (xloc + (frameHeight / 10) <= 0) {
-			xM = 1;
-		}
-    	
-    	if (yloc + imgHeight >= frameHeight) {
-			yM = -1;
-		} else if (yloc + (frameHeight / 10) <= 0) {
-			yM = 1;
-		}
-    	
-    	if (xM == 1 && yM == 1) {
-    		direction = SE;
-    	}
-    	
-    	if (xM == -1 && yM == -1) {
-    		direction = NW;
-    	}
-    	if (xM == 1 && yM == -1) {
-    		direction = NE;
-    	}
-    	
-    	if (xM == -1 && yM == 1){
-    		direction = SW;
-    	}
-    }
+    
 }
--- src/View.java
+++ src/View.java
@@ -1,3 +1,15 @@
+/** Brian Bugieda and Max Luu
+ * 	Lab 4
+ * 
+ * View: Contains everything about graphics and images
+ * Know size of world, which images to load etc
+ *
+ * has methods to
+ * provide boundaries
+ * use proper images for direction
+ * load images for all direction (an image should only be loaded once!!! why?)
+ */
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
@@ -8,34 +20,99 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-/**
- * View: Contains everything about graphics and images
- * Know size of world, which images to load etc
- *
- * has methods to
- * provide boundaries
- * use proper images for direction
- * load images for all direction (an image should only be loaded once!!! why?)
- **/
-
 @SuppressWarnings("serial")
 public class View extends JPanel {
-	
+
 	private int frameWidth = 500;
     private int frameHeight = 300;
     private int imgWidth = 165;
     private int imgHeight = 165;
     
-    BufferedImage[] pics;
-    BufferedImage[][] animations;
+    private BufferedImage[][] animations;
     
-    final int frameCount = 10;
-    final int directionCount = 8;
-    int picNum = 0;
+    private final int frameCount = 10;
+    private final int directionCount = 8;
+    private int picNum = 0;
     
     private int x;
     private int y;
     private int dir;
+
+    /** View() Constructor
+     * Initializes a new frame to view the animation.
+     * Loads orc images into a BufferedImage array.
+     */
+    public View() {
+    	
+    	JFrame frame = new JFrame();
+    	frame.getContentPane().add(this);
+    	frame.setBackground(Color.gray);
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.setSize(frameWidth, frameHeight);
+    	frame.setVisible(true);
+
+    	animations = new BufferedImage[directionCount][frameCount];
+    	BufferedImage[] indexArray = new BufferedImage[directionCount];
+
+    	// Fills an array with the file paths for 8 different orc images
+    	String[] directionArray = {"southeast", "east", "north", "northeast", "northwest", "south", "southwest", "west"};
+
+    	for (int i = 0; i < directionArray.length; i++) {
+    		indexArray[i] = createImage("src/orc_animation/orc_forward_" + directionArray[i] + ".png");
+    	}
+
+    	for (int i = 0; i < directionCount; i++) {
+    		for (int j = 0; j < frameCount; j++) {
+    			animations[i][j] = indexArray[i].getSubimage(imgWidth*j, 0, imgWidth, imgHeight);
+    		}
+    	}
+    }
+    
+    public static void main(String[] args) {
+		
+    	Controller controller = new Controller();
+		controller.start();
+	}
+    
+    private BufferedImage createImage(String name) {
+    	
+    	BufferedImage bufferedImage;
+    	try {
+    		bufferedImage = ImageIO.read(new File(name));
+    		return bufferedImage;
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    	return null;
+	}
+    
+    @Override
+    public void paint(Graphics g) {
+    	
+    	picNum = (picNum + 1) % frameCount;
+    	g.drawImage(animations[this.dir][picNum], this.x, this.y, Color.gray, this);
+    }
+    
+    /**
+     * Updates the position of the orc and repaints the frame with the orc's new position
+     * @param x, the x coordinate of the orc
+     * @param y, the y coordinate of the orc
+     * @param d, the direction the orc is moving
+     */
+    public void update(int x, int y, int d) {
+    	
+    	this.x = x;
+    	this.y = y;
+    	this.dir = d;
+    	
+    	try {
+    		this.setBackground(Color.gray);
+	    	this.repaint();
+	    	Thread.sleep(100);
+    	} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
     
     public int getWidth() {
     	return frameWidth;
@@ -60,65 +137,4 @@ public class View extends JPanel {
     public int getFrameHeight() {
     	return frameHeight;
     }
-    
-    public View() {
-    	
-    	JFrame frame = new JFrame();
-    	frame.getContentPane().add(this);
-    	frame.setBackground(Color.gray);
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	frame.setSize(frameWidth, frameHeight);
-    	frame.setVisible(true);
-
-    	animations = new BufferedImage[directionCount][frameCount];
-    	BufferedImage[] indexArray = new BufferedImage[directionCount];
-    	
-    	String[] directionArray = {"southeast", "east", "north", "northeast", "northwest", "south", "southwest", "west"};
- 
-    	for (int i = 0; i < directionArray.length; i++) {
-    		indexArray[i] = createImage("src/orc_animation/orc_forward_" + directionArray[i] + ".png");
-    	}
-    	
-    	for (int i = 0; i < directionCount; i++) {
-    		for (int j = 0; j < frameCount; j++) {
-				animations[i][j] = indexArray[i].getSubimage(imgWidth*j, 0, imgWidth, imgHeight);
-			}
-    	}
-    }
-    
-    
-    private BufferedImage createImage(String name) {
-    	BufferedImage bufferedImage;
-    	try {
-    		bufferedImage = ImageIO.read(new File(name));
-    		return bufferedImage;
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    	}
-    	return null;
-	}
-    
-    public void paint(Graphics g) {
-    	picNum = (picNum + 1) % frameCount;
-    	g.drawImage(animations[this.dir][picNum], this.x, this.y, Color.gray, this);
-    }
-    
-    public void update(int x, int y, int d) {
-    	this.x = x;
-    	this.y = y;
-    	this.dir = d;
-    	
-    	try {
-    		this.setBackground(Color.gray);
-	    	this.repaint();
-	    	Thread.sleep(100);
-    	} catch (Exception e) {
-			e.printStackTrace();
-		}
-    }
-    
-    public static void main(String[] args) {
-		Controller controller = new Controller();
-		controller.start();
-	}
 }
